6192a22abb6cd6b24527dd1c4cbfc47b810e4437
Nik <nikos_dou@hotmail.com>
2019-08-11 18:43:26 +0300

    update_picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,9 +18,10 @@ class User < ApplicationRecord
 
   def self.find_or_create_from_auth_hash(auth_hash)
     graph=Koala::Facebook::API.new(auth_hash['credentials']['token'])
-    user=find_or_create_by(uid: auth_hash['uid']) do |user|
+    user=find_or_create_by(uid: auth_hash['uid']).tap do |user|
         user.name= auth_hash['info']['name']
         user.picture=graph.get_object("me",{fields:"picture.type(normal){url}"})["picture"]["data"]["url"]
+        user.save!
     end
   end
 
